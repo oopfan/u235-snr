af3a2fd38bc772f0cde79ed0c1725a4424bc6f2d
oopfan <oopfan@optonline.net>
2020-01-25 21:28:26 -0500

    Functional sbcalc, needs styling

--- src/app/sbcalc/sbcalc.component.ts
+++ src/app/sbcalc/sbcalc.component.ts
@@ -6,6 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sbcalc.component.css']
 })
 export class SbcalcComponent implements OnInit {
+  magnitude = '';
+  majorAxis = '';
+  minorAxis = '';
+  surfaceBrightness = '';
+
+  onChangeMagnitude(value: string) {
+    this.magnitude = value;
+    this.calculateSurfaceBrightness();
+  }
+
+  onChangeMajorAxis(value: string) {
+    this.majorAxis = value;
+    this.calculateSurfaceBrightness();
+  }
+
+  onChangeMinorAxis(value: string) {
+    this.minorAxis = value;
+    this.calculateSurfaceBrightness();
+  }
+
+  calculateSurfaceBrightness() {
+    const sb = parseFloat(this.magnitude) + 2.5 * Math.log10(900 * Math.PI * parseFloat(this.majorAxis) * parseFloat(this.minorAxis));
+    this.surfaceBrightness = !isNaN(sb) ? sb.toFixed(1) : '';
+  }
 
   constructor() { }
 
