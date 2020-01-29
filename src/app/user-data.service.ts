ee7de845e8eaf0135cf50baea002e051eb2052c9
oopfan <oopfan@optonline.net>
2020-01-29 07:31:47 -0500

    Add UserTargets sanity check when get storage

--- src/app/user-data.service.ts
+++ src/app/user-data.service.ts
@@ -9,6 +9,48 @@ export class UserDataService {
 
   constructor(private storage: LocalStorageService) { }
 
+  private initTargets() {
+    // get what is in local storage:
+    const obj = this.storage.get('userTargets');
+    // make sure its structure is compatible with downstream code:
+    let isOK = true;
+    if (!obj) {
+      isOK = false;
+    }
+    else {
+      if (!("nextid" in obj) || typeof obj.nextid !== 'number' || !isFinite(obj.nextid)) {
+        isOK = false;
+      }
+      else {
+        if (!("list" in obj) || !Array.isArray(obj.list)) {
+          isOK = false;
+        }
+        else {
+          obj.list.forEach((element: any) => {
+            if (typeof element !== 'object') {
+              isOK = false;
+            }
+            else {
+              if (!("name" in element) || typeof element.name !== 'string') {
+                isOK = false;
+              }
+              else {
+                if (!("surfaceBrightness" in element) || typeof element.surfaceBrightness !== 'string') {
+                  isOK = false;
+                }
+              }
+            }
+          });
+        }
+      }
+    }
+    this.targetsCache = isOK ? obj : { list: [], nextid: 0 };
+  }
+
+  discardTargets() {
+    this.initTargets();
+  }
+
   saveTargets() {
     if (!this.targetsCache) {
       this.initTargets();
@@ -16,23 +58,11 @@ export class UserDataService {
     this.storage.set('userTargets', this.targetsCache);
   }
 
-  discardTargets() {
-    const obj = this.storage.get('userTargets');
-    if (obj) {
-      this.targetsCache = obj;
-    }
-  }
-
   getAllTargets() {
     if (!this.targetsCache) {
       this.initTargets();
     }
     return this.targetsCache.list;
-  }
-
-  private initTargets() {
-    const obj = this.storage.get('userTargets');
-    this.targetsCache = obj || { list: [], nextid: 0 };
   }
 
   createTarget(name: string, surfaceBrightness: string) {
