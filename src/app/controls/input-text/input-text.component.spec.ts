04f4ffc88a1e39d73fb2ad6c705cf57cfa1b728b
oopfan <oopfan@optonline.net>
2020-02-28 20:12:47 -0500

    text-input unit tests

--- src/app/controls/input-text/input-text.component.spec.ts
+++ src/app/controls/input-text/input-text.component.spec.ts
@@ -1,10 +1,12 @@
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NO_ERRORS_SCHEMA, DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { InputTextComponent } from './input-text.component';
+import { By } from '@angular/platform-browser';
 
 describe('InputTextComponent', () => {
   let component: InputTextComponent;
   let fixture: ComponentFixture<InputTextComponent>;
+  let el: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -17,10 +19,22 @@ describe('InputTextComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(InputTextComponent);
     component = fixture.componentInstance;
+    el = fixture.debugElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display label', () => {
+    expect(component).toBeTruthy();
+    const testLabel = 'My Label';
+    component.label = testLabel;
+    fixture.detectChanges();
+    const label = el.query(By.css('.label'));
+    expect(label).toBeTruthy('Could not find label');
+    expect(label.nativeElement.textContent).toBe(testLabel, 'Unexpected label');
+  });
+
 });
