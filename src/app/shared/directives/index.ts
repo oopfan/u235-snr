20197bce3771df6a3b4f99a5bcb995017f70eb35
oopfan <oopfan@optonline.net>
2020-03-15 07:29:40 -0400

    Move directives to shared

---
+++ src/app/shared/directives/index.ts
@@ -0,0 +1,5 @@
+import { HighlightDirective } from './highlight/highlight.directive';
+
+export const directives: any[] = [ HighlightDirective ];
+
+export * from './highlight/highlight.directive';
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import * as fromComponents from './components';
+import * as fromDirectives from './directives';
 
 @NgModule({
-  declarations: [ ...fromComponents.components ],
+  declarations: [ ...fromComponents.components, ...fromDirectives.directives ],
   imports: [ CommonModule, ReactiveFormsModule ],
-  exports: [ ReactiveFormsModule, ...fromComponents.components ]
+  exports: [ ReactiveFormsModule, ...fromComponents.components, ...fromDirectives.directives ]
 })
 export class SharedModule { }
